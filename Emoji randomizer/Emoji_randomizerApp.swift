79a8cb4bf965c023807b3e5b1f8a665ed41fcccc
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-02-11 10:32:16 +0800

    Initial Commit

---
+++ Emoji randomizer/Emoji_randomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_randomizerApp.swift
+//  Emoji randomizer
+//
+//  Created by Nethan on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_randomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
